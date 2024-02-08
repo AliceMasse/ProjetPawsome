1615ecf9964cde86e193686bd4fc4220b4979582
Alice Massé <90843840+AliceMasse@users.noreply.github.com>
2024-02-08 12:35:26 +0100

    Add files via upload

---
+++ welcome1.js
@@ -0,0 +1,29 @@
+<div style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(180deg, #FDFAFA 0%, #FDF8F0 100%)'}}>
+    <div style={{width: 310, left: 41, top: 494, position: 'absolute', color: '#736D6D', fontSize: 20, fontFamily: 'Unkempt', fontWeight: '400', wordWrap: 'break-word'}}>Avant de commencer, on vous explique comment trouver la ou le pet-sitter parfait(e) pour votre compagnon à 4 pattes !<br/><br/>On y va ?</div>
+    <div style={{width: 366.43, height: 328.03, left: 321.94, top: 267.78, position: 'absolute', transform: 'rotate(179.91deg)', transformOrigin: '0 0'}}>
+        <div style={{width: 362, height: 224.71, left: -0.17, top: -103.36, position: 'absolute', transform: 'rotate(178.76deg)', transformOrigin: '0 0', background: '#DDC2FF', boxShadow: '0px 40px 50px 4px rgba(229.78, 205.41, 235.88, 0.40)', borderTopLeftRadius: 400, borderTopRightRadius: 400, borderBottomRightRadius: 116.32, border: '0.40px rgba(150.40, 9.21, 236.94, 0.20) solid'}} />
+    </div>
+    <div style={{width: 310, left: 38, top: 272, position: 'absolute', color: 'black', fontSize: 60, fontFamily: 'Glory', fontWeight: '400', wordWrap: 'break-word'}}>Bienvenue par minou !</div>
+    <div style={{width: 313, left: 38, top: 734, position: 'absolute', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 62, display: 'inline-flex'}}>
+        <div style={{paddingLeft: 24, paddingRight: 24, paddingTop: 16, paddingBottom: 16, background: '#FEFEFF', borderRadius: 64, border: '1px #9609ED solid', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 8, display: 'flex'}}>
+            <div style={{justifyContent: 'flex-start', alignItems: 'flex-start', gap: 8, display: 'flex'}}>
+                <div style={{color: '#9609ED', fontSize: 24, fontFamily: 'Glory', fontWeight: '600', wordWrap: 'break-word'}}>Passer</div>
+            </div>
+        </div>
+        <div style={{flex: '1 1 0', height: 59, paddingLeft: 24, paddingRight: 24, paddingTop: 16, paddingBottom: 16, background: '#9609ED', borderRadius: 64, justifyContent: 'flex-start', alignItems: 'flex-start', gap: 8, display: 'flex'}}>
+            <div style={{flex: '1 1 0', height: 27, justifyContent: 'center', alignItems: 'center', gap: 8, display: 'flex'}}>
+                <div style={{color: '#FDF8F0', fontSize: 24, fontFamily: 'Glory', fontWeight: '600', wordWrap: 'break-word'}}>C’est parti !</div>
+            </div>
+        </div>
+    </div>
+    <div style={{width: 390, height: 47, left: 0, top: 0, position: 'absolute'}}>
+        <div style={{width: 27.23, height: 13, left: 336.20, top: 19.10, position: 'absolute'}}>
+            <div style={{width: 25, height: 13, left: 0, top: 0, position: 'absolute', opacity: 0.35, borderRadius: 4.30, border: '1px black solid'}} />
+            <div style={{width: 1.33, height: 4, left: 25.90, top: 4.67, position: 'absolute', opacity: 0.40, background: 'black'}}></div>
+            <div style={{width: 21, height: 9, left: 2, top: 2, position: 'absolute', background: 'black', borderRadius: 2.50}} />
+        </div>
+        <img style={{width: 17.14, height: 12.10, left: 311.70, top: 19.70, position: 'absolute'}} src="https://via.placeholder.com/17x12" />
+        <img style={{width: 19.20, height: 12, left: 285, top: 19.60, position: 'absolute'}} src="https://via.placeholder.com/19x12" />
+        <div style={{left: 43.50, top: 16, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 17, fontFamily: 'SF Pro', fontWeight: '590', wordWrap: 'break-word'}}>13:13</div>
+    </div>
+</div>
---
+++ welcome3.js
@@ -0,0 +1,119 @@
+<div style={{width: '100%', height: '100%', position: 'relative', background: 'linear-gradient(180deg, #FDFAFA 0%, #FDF8F0 100%)'}}>
+    <div style={{width: 390, paddingTop: 4, paddingBottom: 8, left: 0, top: 773, position: 'absolute', background: 'white', borderTopLeftRadius: 16, borderTopRightRadius: 16, justifyContent: 'center', alignItems: 'flex-end', gap: 24, display: 'inline-flex'}}>
+        <div style={{width: 64, alignSelf: 'stretch', flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center', gap: 6, display: 'inline-flex'}}>
+            <div style={{width: 28, height: 28, position: 'relative'}}>
+                <div style={{width: 2.74, height: 2.73, left: 18.26, top: 18.26, position: 'absolute', background: '#736D6D'}}></div>
+                <div style={{width: 16.19, height: 13.90, left: 10.94, top: 13.23, position: 'absolute', background: '#736D6D'}}></div>
+                <div style={{width: 21, height: 21, left: 0.88, top: 0.88, position: 'absolute', background: '#736D6D'}}></div>
+                <div style={{width: 0.88, height: 0.88, left: 18.81, top: 10.94, position: 'absolute', background: '#736D6D'}}></div>
+            </div>
+            <div style={{color: '#736D6D', fontSize: 14, fontFamily: 'Glory', fontWeight: '400', wordWrap: 'break-word'}}>Rechercher</div>
+        </div>
+        <div style={{width: 64, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{width: 42, height: 42, position: 'relative'}}>
+                <div style={{width: 10.52, height: 5.95, left: 15.56, top: 16.33, position: 'absolute', background: '#736D6D'}}></div>
+                <div style={{width: 33.21, height: 32.58, left: 4.39, top: 4.71, position: 'absolute', background: '#736D6D'}}></div>
+            </div>
+            <div style={{color: '#736D6D', fontSize: 14, fontFamily: 'Glory', fontWeight: '400', wordWrap: 'break-word'}}>Accueil</div>
+        </div>
+        <div style={{flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center', display: 'inline-flex'}}>
+            <div style={{width: 39, height: 39, position: 'relative', transform: 'rotate(180deg)', transformOrigin: '0 0'}}>
+                <div style={{width: 26.31, height: 28.71, left: 6.35, top: 5.14, position: 'absolute', background: '#9609ED'}}></div>
+                <div style={{width: 4.41, height: 0.91, left: 15.57, top: 28.95, position: 'absolute', background: '#9609ED'}}></div>
+                <div style={{width: 4.34, height: 0.91, left: 19.82, top: 31.87, position: 'absolute', transform: 'rotate(-169.13deg)', transformOrigin: '0 0', background: '#9609ED'}}></div>
+                <div style={{width: 1.22, height: 1.37, left: 14.97, top: 25.87, position: 'absolute', background: '#9609ED'}}></div>
+                <div style={{width: 1.52, height: 1.67, left: 20.35, top: 8.74, position: 'absolute', background: '#9609ED'}}></div>
+            </div>
+            <div style={{width: 64, textAlign: 'center', color: '#9609ED', fontSize: 14, fontFamily: 'Glory', fontWeight: '600', wordWrap: 'break-word'}}>Mon Filoo</div>
+        </div>
+        <div style={{width: 64, flexDirection: 'column', justifyContent: 'flex-end', alignItems: 'center', gap: 5, display: 'inline-flex'}}>
+            <div style={{width: 33, height: 33, position: 'relative'}}>
+                <div style={{width: 22.81, height: 28.88, left: 5.10, top: 2.06, position: 'absolute', background: '#736D6D'}}></div>
+            </div>
+            <div style={{color: '#736D6D', fontSize: 14, fontFamily: 'Glory', fontWeight: '400', wordWrap: 'break-word'}}>Mon compte</div>
+        </div>
+    </div>
+    <div style={{left: 79, top: 69, position: 'absolute', justifyContent: 'flex-start', alignItems: 'center', gap: 25, display: 'inline-flex'}}>
+        <div style={{width: 39, height: 39, position: 'relative'}}>
+            <div style={{width: 16.35, height: 14.01, left: 11.33, top: 16.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 14.01, height: 11.67, left: 12.50, top: 17.75, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 7.24, top: 13.08, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 8.41, top: 14.25, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 13.08, top: 8.41, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 14.25, top: 9.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 20.08, top: 8.41, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 21.25, top: 9.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 25.92, top: 13.08, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 27.09, top: 14.25, position: 'absolute', background: '#D9D9D9'}}></div>
+        </div>
+        <div style={{width: 39, height: 39, position: 'relative'}}>
+            <div style={{width: 16.35, height: 14.01, left: 11.33, top: 16.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 14.01, height: 11.67, left: 12.50, top: 17.75, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 7.24, top: 13.08, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 8.41, top: 14.25, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 13.08, top: 8.41, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 14.25, top: 9.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 20.08, top: 8.41, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 21.25, top: 9.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 25.92, top: 13.08, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 27.09, top: 14.25, position: 'absolute', background: '#D9D9D9'}}></div>
+        </div>
+        <div style={{width: 39, height: 39, position: 'relative'}}>
+            <div style={{width: 16.35, height: 14.01, left: 11.33, top: 16.58, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 14.01, height: 11.67, left: 12.50, top: 17.75, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 5.84, height: 7, left: 7.24, top: 13.08, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 8.41, top: 14.25, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 5.84, height: 7, left: 13.08, top: 8.41, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 14.25, top: 9.58, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 5.84, height: 7, left: 20.08, top: 8.41, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 21.25, top: 9.58, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 5.84, height: 7, left: 25.92, top: 13.08, position: 'absolute', background: '#332002'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 27.09, top: 14.25, position: 'absolute', background: '#332002'}}></div>
+        </div>
+        <div style={{width: 39, height: 39, position: 'relative'}}>
+            <div style={{width: 16.35, height: 14.01, left: 11.33, top: 16.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 14.01, height: 11.67, left: 12.50, top: 17.75, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 7.24, top: 13.08, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 8.41, top: 14.25, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 13.08, top: 8.41, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 14.25, top: 9.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 20.08, top: 8.41, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 21.25, top: 9.58, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 5.84, height: 7, left: 25.92, top: 13.08, position: 'absolute', background: '#D9D9D9'}}></div>
+            <div style={{width: 3.50, height: 4.67, left: 27.09, top: 14.25, position: 'absolute', background: '#D9D9D9'}}></div>
+        </div>
+    </div>
+    <div style={{width: 390, height: 384, left: 0, top: 300, position: 'absolute'}}>
+        <div style={{width: 390, height: 384, left: 0, top: 0, position: 'absolute', background: '#DDC2FF', borderRadius: 9999}} />
+        <div style={{width: 299, left: 64, top: 145, position: 'absolute'}}><span style="color: '#332002', fontSize: 20, fontFamily: 'Unkempt', fontWeight: '400', wordWrap: 'break-word'">Le plus important :</span><span style="color: '#332002', fontSize: 20, fontFamily: 'Unkempt', fontWeight: '700', wordWrap: 'break-word'"> ton Filoo</span><span style="color: '#332002', fontSize: 20, fontFamily: 'Unkempt', fontWeight: '400', wordWrap: 'break-word'"> !<br/><br/>Tu pourras y renseigner toutes les infos relatives à ton animal de compagnie, afin qu’il ou elle puisse trouver LE match parfait comme pet-sitter !</span></div>
+    </div>
+    <div style={{width: 114, height: 114, left: 244, top: 319, position: 'absolute', transform: 'rotate(180deg)', transformOrigin: '0 0'}}>
+        <div style={{width: 76.90, height: 83.93, left: 18.55, top: 15.03, position: 'absolute', background: 'white'}}></div>
+        <div style={{width: 12.89, height: 2.67, left: 45.50, top: 84.61, position: 'absolute', background: 'white'}}></div>
+        <div style={{width: 12.68, height: 2.67, left: 57.92, top: 93.15, position: 'absolute', transform: 'rotate(-169.13deg)', transformOrigin: '0 0', background: 'white'}}></div>
+        <div style={{width: 3.56, height: 4, left: 43.75, top: 75.61, position: 'absolute', background: 'white'}}></div>
+        <div style={{width: 4.45, height: 4.89, left: 59.48, top: 25.53, position: 'absolute', background: 'white'}}></div>
+    </div>
+    <div style={{width: 313, left: 38, top: 148, position: 'absolute', justifyContent: 'center', alignItems: 'flex-start', gap: 106, display: 'inline-flex'}}>
+        <div style={{paddingLeft: 24, paddingRight: 24, paddingTop: 16, paddingBottom: 16, background: '#FEFEFF', borderRadius: 64, border: '1px #9609ED solid', justifyContent: 'flex-start', alignItems: 'flex-start', gap: 8, display: 'flex'}}>
+            <div style={{justifyContent: 'flex-start', alignItems: 'flex-start', gap: 8, display: 'flex'}}>
+                <div style={{color: '#9609ED', fontSize: 24, fontFamily: 'Glory', fontWeight: '600', wordWrap: 'break-word'}}>Passer</div>
+            </div>
+        </div>
+        <div style={{paddingLeft: 24, paddingRight: 24, paddingTop: 16, paddingBottom: 16, background: '#9609ED', borderRadius: 64, justifyContent: 'flex-start', alignItems: 'flex-start', gap: 8, display: 'flex'}}>
+            <div style={{justifyContent: 'flex-start', alignItems: 'flex-start', gap: 8, display: 'flex'}}>
+                <div style={{color: '#FDF8F0', fontSize: 24, fontFamily: 'Glory', fontWeight: '600', wordWrap: 'break-word'}}>Suivant</div>
+            </div>
+        </div>
+    </div>
+    <div style={{width: 390, height: 47, left: 0, top: 0, position: 'absolute'}}>
+        <div style={{width: 27.23, height: 13, left: 336.20, top: 19.10, position: 'absolute'}}>
+            <div style={{width: 25, height: 13, left: 0, top: 0, position: 'absolute', opacity: 0.35, borderRadius: 4.30, border: '1px black solid'}} />
+            <div style={{width: 1.33, height: 4, left: 25.90, top: 4.67, position: 'absolute', opacity: 0.40, background: 'black'}}></div>
+            <div style={{width: 21, height: 9, left: 2, top: 2, position: 'absolute', background: 'black', borderRadius: 2.50}} />
+        </div>
+        <img style={{width: 17.14, height: 12.10, left: 311.70, top: 19.70, position: 'absolute'}} src="https://via.placeholder.com/17x12" />
+        <img style={{width: 19.20, height: 12, left: 285, top: 19.60, position: 'absolute'}} src="https://via.placeholder.com/19x12" />
+        <div style={{left: 43.50, top: 16, position: 'absolute', textAlign: 'center', color: 'black', fontSize: 17, fontFamily: 'SF Pro', fontWeight: '590', wordWrap: 'break-word'}}>13:13</div>
+    </div>
+</div>
